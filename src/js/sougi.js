7c0d7cb9e7c5430d2c227bcab084e9b4eba4ca7a
s-t-00 <coollife.swim.0215@gmail.com>
2020-07-01 18:13:30 +0900

    作業

---
+++ src/js/sougi.js
@@ -0,0 +1,39 @@
+< script >
+    $(function() {
+        $(".menu-btn01").click(function() {
+            $(".sp-menu01").slideToggle(200);
+        });
+        $(".sp-menu01 li a").click(function() {
+            $(".sp-menu01").css({
+                display: "none"
+            });
+        });
+        $('.menu-btn01').click(function() {
+            event.stopPropagation();
+        });
+        $(document).click(function(event) {
+            if (!$(event.target).closest('.sp-menu01').length) {
+                $('.sp-menu01').slideUp(200);
+            }
+        });
+    });
+
+$(function() {
+    $(".menu-btn02").click(function() {
+        $(".sp-menu02").slideToggle(200);
+    });
+    $(".sp-menu02 li a").click(function() {
+        $(".sp-menu02").css({
+            display: "none"
+        });
+    });
+    $('.menu-btn02').click(function() {
+        event.stopPropagation();
+    });
+    $(document).click(function(event) {
+        if (!$(event.target).closest('.sp-menu02').length) {
+            $('.sp-menu02').slideUp(200);
+        }
+    });
+}); <
+/script>
