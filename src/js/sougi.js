8bbd5b6efacd3aa430e3b959fe181aeb2d6eb6ab
s-t-00 <coollife.swim.0215@gmail.com>
2020-07-03 11:21:04 +0900

    no message

--- src/js/sougi.js
+++ src/js/sougi.js
@@ -1,41 +1,53 @@
-{
-  /* < script >
-    $(function() {
-        $(".menu-btn01").click(function() {
-            $(".sp-menu01").slideToggle(200);
+var $win = $(window);
+
+$win.on("load resize", function () {
+  var windowWidth = window.innerWidth;
+
+  if (windowWidth > 1024) {
+    // PCの処理
+  } else if (windowWidth > 768) {
+    // TABの処理
+  } else {
+    // SPの処理
+    $(function () {
+      $(".menu-btn01").click(function () {
+        $(".sp-menu01").slideToggle(200);
+      });
+      $(".sp-menu01 li a").click(function () {
+        $(".sp-menu01").css({
+          display: "none",
         });
-        $(".sp-menu01 li a").click(function() {
-            $(".sp-menu01").css({
-                display: "none"
-            });
-        });
-        $('.menu-btn01').click(function() {
-            event.stopPropagation();
-        });
-        $(document).click(function(event) {
-            if (!$(event.target).closest('.sp-menu01').length) {
-                $('.sp-menu01').slideUp(200);
-            }
-        });
+      });
+      $(".menu-btn01").click(function () {
+        event.stopPropagation();
+      });
+      $(document).click(function (event) {
+        if (!$(event.target).closest(".sp-menu01").length) {
+          $(".sp-menu01").slideUp(200);
+        }
+      });
     });
 
-$(function() {
-    $(".menu-btn02").click(function() {
+    $(function () {
+      $(".menu-btn02").click(function () {
         $(".sp-menu02").slideToggle(200);
-    });
-    $(".sp-menu02 li a").click(function() {
+      });
+      $(".sp-menu02 li a").click(function () {
         $(".sp-menu02").css({
-            display: "none"
+          display: "none",
         });
-    });
-    $('.menu-btn02').click(function() {
+      });
+      $(".menu-btn02").click(function () {
         event.stopPropagation();
-    });
-    $(document).click(function(event) {
-        if (!$(event.target).closest('.sp-menu02').length) {
-            $('.sp-menu02').slideUp(200);
+      });
+      $(document).click(function (event) {
+        if (!$(event.target).closest(".sp-menu02").length) {
+          $(".sp-menu02").slideUp(200);
         }
+      });
     });
-}); <
-/script> */
-}
+  }
+});
+
+/*
+ */
