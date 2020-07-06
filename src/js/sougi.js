321f36e7d2a4a4f660dff7fa5e2a0ca45b04af59
s-t-00 <coollife.swim.0215@gmail.com>
2020-07-06 19:55:41 +0900

    no message

--- src/js/sougi.js
+++ src/js/sougi.js
@@ -1,52 +1,75 @@
 var $win = $(window);
 
 $win.on("load resize", function () {
-  var windowWidth = window.innerWidth;
+    var windowWidth = window.innerWidth;
 
-  if (windowWidth > 1024) {
-    // PCの処理
-  } else if (windowWidth > 768) {
-    // TABの処理
-  } else {
-    // SPの処理
-    $(function () {
-      $(".menu-btn01").click(function () {
-        $(".sp-menu01").slideToggle(200);
-      });
-      $(".sp-menu01 li a").click(function () {
-        $(".sp-menu01").css({
-          display: "none",
-        });
-      });
-      $(".menu-btn01").click(function () {
-        event.stopPropagation();
-      });
-      $(document).click(function (event) {
-        if (!$(event.target).closest(".sp-menu01").length) {
-          $(".sp-menu01").slideUp(200);
-        }
-      });
-    });
+    if (windowWidth > 1024) {
+        // PCの処理
+    } else if (windowWidth > 768) {
+        // TABの処理
+    } else {
+        // SPの処理
 
-    $(function () {
-      $(".menu-btn02").click(function () {
-        $(".sp-menu02").slideToggle(200);
-      });
-      $(".sp-menu02 li a").click(function () {
-        $(".sp-menu02").css({
-          display: "none",
+
+
+
+
+
+
+        $(function () {
+            $(".accordion li a").on("click", function () {
+                $(this).next().slideToggle();
+                // activeが存在する場合
+                if ($(this).children(".accordion_icon").hasClass('active')) {
+                    // activeを削除
+                    $(this).children(".accordion_icon").removeClass('active');
+                } else {
+                    // activeを追加
+                    $(this).children(".accordion_icon").addClass('active');
+                }
+            });
         });
-      });
-      $(".menu-btn02").click(function () {
-        event.stopPropagation();
-      });
-      $(document).click(function (event) {
-        if (!$(event.target).closest(".sp-menu02").length) {
-          $(".sp-menu02").slideUp(200);
-        }
-      });
-    });
-  }
+
+
+
+        // $(function () {
+        //     $(".menu-btn01").click(function () {
+        //         $(".sp-menu01").slideToggle(200);
+        //     });
+        //     $(".sp-menu01 li a").click(function () {
+        //         $(".sp-menu01").css({
+        //             display: "none",
+        //         });
+        //     });
+        //     $(".menu-btn01").click(function () {
+        //         event.stopPropagation();
+        //     });
+        //     $(document).click(function (event) {
+        //         if (!$(event.target).closest(".sp-menu01").length) {
+        //             $(".sp-menu01").slideUp(200);
+        //         }
+        //     });
+        // });
+
+        // $(function () {
+        //     $(".menu-btn02").click(function () {
+        //         $(".sp-menu02").slideToggle(200);
+        //     });
+        //     $(".sp-menu02 li a").click(function () {
+        //         $(".sp-menu02").css({
+        //             display: "none",
+        //         });
+        //     });
+        //     $(".menu-btn02").click(function () {
+        //         event.stopPropagation();
+        //     });
+        //     $(document).click(function (event) {
+        //         if (!$(event.target).closest(".sp-menu02").length) {
+        //             $(".sp-menu02").slideUp(200);
+        //         }
+        //     });
+        // });
+    }
 });
 
 /*
